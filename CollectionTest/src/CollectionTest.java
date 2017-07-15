4c71c9c0912dbe3abec30f9a25c6bf7fc2737245
SauravYadav12 <saurav1286@gmail.com>
2017-07-15 15:17:04 +0530

    Add files via upload

---
+++ CollectionTest/src/CollectionTest.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class CollectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Collection>  s=new ArrayList<Collection>();
+		s.add(new Collection(101, "saurav", "mathes"));
+		s.add(new Collection(101, "saurav", "mathes"));
+		s.add(new Collection(103, "pramod", "bio"));
+		s.add(new Collection(104, "abhishek", "mathes"));
+		s.add(new Collection(101, "saurav", "mathes"));
+		
+		 List<Collection> list= new ArrayList();
+		 
+		  list.addAll(s);
+		  
+		  
+		   Set empIds= new HashSet();//flag that keeps employee ids
+		  
+		  for(Iterator it=list.listIterator();it.hasNext();){
+			  Collection stu=(Collection)it.next();
+		  
+		   
+		   if(empIds.add(stu.getRoll_no())==false){//if found duplicate remove from the list
+		    it.remove();
+		   }
+		  }
+		  
+		  for(Collection stu:list){
+		   System.out.println(stu.getRoll_no());
+		   System.out.println(stu.getName());
+		  }
+
+	}
+
+}
